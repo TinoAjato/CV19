0b7295e99eb79f1047daaa1db13d9871b4db4512
Tino Ajato <a-belia1993@yandex.ru>
2023-04-26 16:34:37 +0300

    Запрос к удаленному серверу для получения сырых данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace CV19Console
 {
     internal class Program
     {
+        const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main( string[] args )
         {
-            Console.Read();
+            var client = new HttpClient();
+            var response = client.GetAsync( data_url ).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+
+
+            Console.ReadLine();
         }
     }
 }
