a39e9c16d07d3b3bd953220f0d4af6660ed2d516
Tino Ajato <a-belia1993@yandex.ru>
2023-04-27 15:09:39 +0300

    Пример с управлением вкладками через модель-представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -10,6 +10,15 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region SelectedPageIndex:int - Номер выбранной вкладки
+        private int _SelectedPageIndex;
+        /// <summary>Номер выбранной вкладки</summary>
+        public int SelectedPageIndex {
+            get => _SelectedPageIndex;
+            set => Set( ref _SelectedPageIndex, value );
+        }
+        #endregion
+
         #region DataPoint:IEnumerable<DataPoint> - Набор данных для визуализации
         private IEnumerable<DataPoint> _TestDataPoint;
         /// <summary>Набор данных для визуализации</summary>
@@ -41,10 +50,24 @@ namespace CV19.ViewModels
 
         #region CloseApplicationCommand
         public ICommand CloseApplicationCommand { get; }
-        private bool CanCloseApplicationCommandExecute( object sender ) => true;
-        private void OnCloseApplicationCommandExecuted( object sender )
+        private bool CanCloseApplicationCommandExecute( object p ) => true;
+        private void OnCloseApplicationCommandExecuted( object p )
         {
             Application.Current.Shutdown();
+        }
+        #endregion
+
+
+        #region ChangetTabIndexCommand
+        public ICommand ChangetTabIndexCommand { get; }
+        private bool CanChangetTabIndexCommandExecute( object p ) => SelectedPageIndex >= 0;
+        private void OnChangetTabIndexCommandExecuted( object p )
+        {
+            if (p is null)
+            {
+                return;
+            }
+            SelectedPageIndex += Convert.ToInt32( p );
         }
         #endregion
 
@@ -55,6 +78,7 @@ namespace CV19.ViewModels
             #region Команды
 
             CloseApplicationCommand = new LambdaCommand( OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute );
+            ChangetTabIndexCommand = new LambdaCommand( OnChangetTabIndexCommandExecuted, CanChangetTabIndexCommandExecute );
 
             #endregion
 
