3f82cd5e161d143dfd4bc9f22887e39ee833cd3f
Tino Ajato <a-belia1993@yandex.ru>
2023-04-28 14:09:07 +0300

    Визуализация даты с помощью шаблона

--- CV19/Models/Decanat/Student.cs
+++ CV19/Models/Decanat/Student.cs
@@ -1,26 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CV19.Models.Decanat
 {
     internal class Student
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Patronymic { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Patronymic { get; set; }
         public DateTime Birthday { get; set; }
         public double Rating { get; set; }
-
-
+        public string? Description { get; set; }
     }
 
     internal class Group
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public ICollection<Student> Students { get; set; }
-
+        public string? Description { get; set; }
     }
 }
