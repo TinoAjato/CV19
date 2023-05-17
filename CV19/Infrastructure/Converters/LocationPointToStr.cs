1e78f8ce741bd1e70ad3745c12335ce57da9088e
Tino Ajato <a-belia1993@yandex.ru>
2023-05-17 16:25:16 +0300

    Конвертеры, ресурсы кистей, блокировка Freezable-объектов в разметке для повышения быстродействия

---
+++ CV19/Infrastructure/Converters/LocationPointToStr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class LocationPointToStr : IValueConverter
+    {
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            if (value is not Point point)
+            {
+                return null;
+            }
+
+            return $"Lat:{point.X};Lon:{point.Y}";
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            if (value is not string str)
+            {
+                return null;
+            }
+
+            var components = str.Split( ';' );
+
+            var latitude_str = components[0].Split( ':' )[1];
+            var longitude_str = components[1].Split( ':' )[1];
+
+            var latitude = double.NaN;
+            double.TryParse( latitude_str, NumberStyles.AllowDecimalPoint, new CultureInfo( "en-us" ), out latitude );
+
+            var longitude = double.NaN;
+            double.TryParse( longitude_str, NumberStyles.AllowDecimalPoint, new CultureInfo( "en-us" ), out longitude );
+
+            return new Point( latitude, longitude );
+        }
+    }
+}
