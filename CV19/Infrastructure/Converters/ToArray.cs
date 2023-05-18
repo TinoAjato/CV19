44a565841b7e636dd5f2f2edf3455aaaa9e9a2db
Tino Ajato <a-belia1993@yandex.ru>
2023-05-18 15:11:24 +0300

    Мульти привязка

---
+++ CV19/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class ToArray : MultiConverter
+    {
+        public override object Convert( object[] values, Type targetType, object parameter, CultureInfo culture )
+        {
+            var collection = new CompositeCollection();
+            foreach (var obj in values)
+            {
+                collection.Add( obj );
+            }
+            return collection;
+        }
+
+        //public override object[] ConvertBack( object value, Type[] targetTypes, object parameter, CultureInfo culture )
+        //{
+        //    return value as object[];
+        //}
+    }
+}
